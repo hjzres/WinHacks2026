a155c80e2797d7f126e812b0afec057057283c8d
hjzr <andreistupariu.as@gmail.com>
2026-02-07 14:58:46 -0500

    changed the sizes of the input boxes

--- wh2026-client/src/lib/Lobby.svelte
+++ wh2026-client/src/lib/Lobby.svelte
@@ -28,11 +28,6 @@
         height:100%;
     }
 
-    .settings{
-        width:25vw;
-        height:100%;
-    }
-
     .title{
         font-size: 2rem;
     }
@@ -52,8 +47,17 @@
         margin-bottom: 33%;
     }
 
-    .menu .section p input{
+    input{
         width:2vw;
+    }
+
+    .settings{
+        width:25vw;
+        height:100%;
+    }
+
+    #time{
+        width: 3vw;
     }
 </style>
 
@@ -69,26 +73,26 @@
             <p class="title">Question Types:</p>
             <div class="sections">
                 <div class="section">
-                    <p><input type="number" name="vc" id="vc">Vector Calculus</p>
-                    <p><input type="number" name="i" id="i">Integration</p>
-                    <p><input type="number" name="d" id="d">Derivatives</p>
+                    <p><input type="number" name="vc" id="vc" value=0>Vector Calculus</p>
+                    <p><input type="number" name="i" id="i" value=0>Integration</p>
+                    <p><input type="number" name="d" id="d" value=0>Derivatives</p>
                 </div>
                 <div class="section">
-                    <p><input type="number" name="f" id="f">Functions</p>
-                    <p><input type="number" name="ba" id="ba">Boolean Algebra</p>
-                    <p><input type="number" name="la" id="la">Linear Algebra</p>
+                    <p><input type="number" name="f" id="f" value=0>Functions</p>
+                    <p><input type="number" name="ba" id="ba" value=0>Boolean Algebra</p>
+                    <p><input type="number" name="la" id="la" value=0>Linear Algebra</p>
                 </div>
                 <div class="section">
-                    <p><input type="number" name="a" id="a">Arithmetic</p>
-                    <p><input type="number" name="t" id="t">Trigonometry</p>
-                    <p><input type="number" name="s" id="s">Statistics</p>
+                    <p><input type="number" name="a" id="a" value=0>Arithmetic</p>
+                    <p><input type="number" name="t" id="t" value=0>Trigonometry</p>
+                    <p><input type="number" name="s" id="s" value=0>Statistics</p>
                 </div>
             </div>
         </div>
         <div class="settings">
             <p class="title">Settings:</p>
-            <p><input type="number" name="time" id="time">s</p>
-            <p><input type="number" name="max-questions" id="max-questions"> questions</p>
+            <p><input type="number" name="time" id="time" value=120>s</p>
+            <p><input type="number" name="max-questions" id="max-questions" value=5> questions</p>
         </div>
     </div>
 </div>
