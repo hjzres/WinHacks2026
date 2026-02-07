1ccaea1eb13def6293d292231c76eacb8145bb5c
hjzr <andreistupariu.as@gmail.com>
2026-02-07 15:49:58 -0500

    Layout done

--- wh2026-client/src/lib/Lobby.svelte
+++ wh2026-client/src/lib/Lobby.svelte
@@ -1,4 +1,64 @@
-<style lang="typescript">
+<script lang="ts">
+
+</script>
+
+<div class="container">
+    <div class="code">
+        <p class="title">CODE :######</p>
+    </div>
+    <div class="players-menu">
+        <p class="title">Player:</p>
+        <div class="sections">
+            <div class="section">
+                <p>---</p>
+                <p>---</p>
+                <p>---</p>
+                <p>---</p>
+            </div>
+            <div class="section">
+                <p>---</p>
+                <p>---</p>
+                <p>---</p>
+                <p>---</p>
+            </div>
+            <div class="section">
+                <p>---</p>
+                <p>---</p>
+                <p>---</p>
+                <p>---</p>
+            </div>
+        </div>
+    </div>
+    <div class=menu>
+        <div class="question-types">
+            <p class="title">Question Types:</p>
+            <div class="sections">
+                <div class="section">
+                    <p><input type="number" name="vc" id="vc" min=0 value=0>Vector Calculus</p>
+                    <p><input type="number" name="i" id="i" min=0 value=0>Integration</p>
+                    <p><input type="number" name="d" id="d" min=0 value=0>Derivatives</p>
+                </div>
+                <div class="section">
+                    <p><input type="number" name="f" id="f" min=0 value=0>Functions</p>
+                    <p><input type="number" name="ba" id="ba" min=0 value=0>Boolean Algebra</p>
+                    <p><input type="number" name="la" id="la" min=0 value=0>Linear Algebra</p>
+                </div>
+                <div class="section">
+                    <p><input type="number" name="a" id="a" min=0 value=0>Arithmetic</p>
+                    <p><input type="number" name="t" id="t" min=0 value=0>Trigonometry</p>
+                    <p><input type="number" name="s" id="s" min=0 value=0>Statistics</p>
+                </div>
+            </div>
+        </div>
+        <div class="settings">
+            <p class="title">Settings:</p>
+            <p><input type="number" name="time" id="time" min=30 value=120>s</p>
+            <p><input type="number" name="max-questions" id="max-questions" min=5 value=5> questions</p>
+        </div>
+    </div>
+</div>
+
+<style>
     .container{
         width:100vw;
         height: 100vh;
@@ -14,6 +74,26 @@
     .players-menu{
         width:100vw;
         height:40vh;
+    }
+
+    .players-menu .sections{
+        display: flex;
+        justify-content: space-around;
+        flex-direction: column;
+        gap:40px;
+    }
+
+    .players-menu .section{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        width: 100vw;
+    }
+
+    .players-menu .section p{
+        display:flex;
+        width: 25vw;
+        justify-content: center;
     }
 
     .menu{
@@ -60,39 +140,3 @@
         width: 3vw;
     }
 </style>
-
-<div class="container">
-    <div class="code">
-        <p class="title">CODE :######</p>
-    </div>
-    <div class="players-menu">
-        <p class="title">Player:</p>
-    </div>
-    <div class=menu>
-        <div class="question-types">
-            <p class="title">Question Types:</p>
-            <div class="sections">
-                <div class="section">
-                    <p><input type="number" name="vc" id="vc" value=0>Vector Calculus</p>
-                    <p><input type="number" name="i" id="i" value=0>Integration</p>
-                    <p><input type="number" name="d" id="d" value=0>Derivatives</p>
-                </div>
-                <div class="section">
-                    <p><input type="number" name="f" id="f" value=0>Functions</p>
-                    <p><input type="number" name="ba" id="ba" value=0>Boolean Algebra</p>
-                    <p><input type="number" name="la" id="la" value=0>Linear Algebra</p>
-                </div>
-                <div class="section">
-                    <p><input type="number" name="a" id="a" value=0>Arithmetic</p>
-                    <p><input type="number" name="t" id="t" value=0>Trigonometry</p>
-                    <p><input type="number" name="s" id="s" value=0>Statistics</p>
-                </div>
-            </div>
-        </div>
-        <div class="settings">
-            <p class="title">Settings:</p>
-            <p><input type="number" name="time" id="time" value=120>s</p>
-            <p><input type="number" name="max-questions" id="max-questions" value=5> questions</p>
-        </div>
-    </div>
-</div>
