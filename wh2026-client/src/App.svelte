aae49e5bc2fd6cf124da476b67c96f005a8dc7bd
Seb18456 <sebastian18678@gmail.com>
2026-02-07 18:28:32 -0500

    Renamed Some Stuff

--- wh2026-client/src/App.svelte
+++ wh2026-client/src/App.svelte
@@ -2,20 +2,20 @@
   import JoinHost from './lib/JoinHost.svelte'
   import Lobby from './lib/Lobby.svelte' 
 
-  import { io } from "socket.io-client";
+  // import { io } from "socket.io-client";
 
-  const socket = io("http://localhost:5000",{
-  auth: {
-    id: "a0472aba-6249-4e24-8baf-711e85d7a58b"
-  }
-  });
+  // const socket = io("http://localhost:5000",{
+  // auth: {
+  //   id: "a0472aba-6249-4e24-8baf-711e85d7a58b"
+  // }
+  // });
 
-  socket.on("connect", () => {
-  socket.emit("create_game", ({status, game_code, name}) => {
-      console.log(status, game_code, name);
-      socket.emit("set_name", "awesomename");
-  });
-  });
+  // socket.on("connect", () => {
+  // socket.emit("create_game", ({status, game_code, name}) => {
+  //     console.log(status, game_code, name);
+  //     socket.emit("set_name", "awesomename");
+  // });
+  // });
 
 </script>
 
--- wh2026-client/src/lib/JoinHost.svelte
+++ wh2026-client/src/lib/JoinHost.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-    import { io } from "socket.io-client";
-
-    const socket = io("http://localhost:5000",{
-    auth: {
-        id: "a0472aba-6249-4e24-8baf-711e85d7a58b"
-    }
-    });
-
-    socket.on("connect", () => {
-    socket.emit("create_game", ({status, game_code, name}) => {
-        console.log(status, game_code, name);
-
-        socket.emit("set_name", "awesomename");
-    });
-    });
-
+    
 </script>
 
 <div id="body">
@@ -25,28 +10,19 @@
         </div>
 
         <div id="site-buttons">
-            <div class="site-rows">
+            <div class="row-layout">
                 <div class="column-layout">
-                    <input type="text" name="code" id="code" placeholder="Enter the code">
+                    <input type="text" name="code" id="code-input-box" placeHolder="Enter the code">
                 </div>
                 <div class="column-layout">
                     <button class="menu-buttons">Join Lobby</button>
                 </div>
             </div>
 
-            <div class="site-rows">
+            <div class="row-layout">
                 <button class="menu-buttons">Host Lobby</button>
             </div>
         </div>
-
-        <!-- <div id="site-components">
-            <p id="site-name">Site Name</p>
-            <p id="site-description">Harness the potential of competitive group studying to solidify your mathematical foundations! Choose an option below:</p>
-        </div>
-        <div id="site-components">
-            <button class="menu-buttons">Join Lobby</button>
-            <button class="menu-buttons">Host Lobby</button>
-        </div> -->
     </div>
 </div>
 
@@ -75,15 +51,6 @@
         border-radius: 15px;
     }
     
-    /* 
-    #site-components {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 50vw;
-    } */
-
     .column-layout {
         width: 100%; 
         height: 50%;
@@ -98,7 +65,7 @@
         flex-direction: row;
     }
     
-    .site-rows {
+    .row-layout {
         width: 50%;
         height: 100%;
     }
