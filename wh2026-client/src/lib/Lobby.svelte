ec98eca102e17b93b4da26993664b51ecbaf95b5
Seb18456 <sebastian18678@gmail.com>
2026-02-07 22:58:05 -0500

    Re-formatted Lobby

--- wh2026-client/src/lib/Lobby.svelte
+++ wh2026-client/src/lib/Lobby.svelte
@@ -2,21 +2,111 @@
     let { players, gameCode } = $props();
 </script>
 
-<div style="background-color: black;">
-    <div style="width: 100vw; height: 100vh; margin: 1vh 1vw 1vh 1vw;">
-        <div class="code-container" style="margin: 1vh 1vw 1vh 1vw;">
-            <p class="title">Join Code: {gameCode}</p>
+<div id="parent-container">
+    <div id="code-container">
+        <div class="text-container">
+            <p style="font-size: 2rem;">Join Code: {gameCode}</p>
         </div>
+    </div>
+
+    <div class="player-list-menu">
+        <div style="width: 100%; height: 20%;">
+            <div class="text-container">
+                <p style="font-size: 2rem">Players:</p>
+            </div>
+        </div>
+
+        <div style="width: 100%; height: 80%; background-color: green;">
+            <div class="player-grid">
+                {#each players as player (player.id ?? player.name)}
+                <p style="margin: 0;">{player.name}</p>
+                {/each}
+            </div>
+        </div>
+    </div>
+
+    <div class="info-menu-container">
+        <div style="width: 100%; height: 20%;">
+            <div style="display: flex; flex-direction: row; align-items: center;">
+                <div style="width: 75%; height: 100%">    
+                    <div class="text-container">
+                        <p style="font-size: 2rem;">Question Types:</p>
+                    </div>
+                </div>
+
+                <div style="width: 25%; height: 100%">
+                    <div class="text-container">
+                        <p style="font-size: 2rem;">Settings:</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div style="width: 100%; height: 80%; display: flex; flex-direction: row; align-items: center;">
+            <div style="width: 75%; height: 100%">
+                <div id="questions-menu">
+                    <!-- CATEGORY 1 -->
+                    <div style="background-color: red; width: 33.4%; height: 100%;">
+                        <p class="mathematical-subject-text"><input type="number" name="i" id="i" min=0 value=0 style="width: 70px; text-align: center;"> Integration</p>
+                        <p class="mathematical-subject-text"><input type="number" name="d" id="d" min=0 value=0 style="width: 70px; text-align: center;"> Derivatives</p>
+                        <p class="mathematical-subject-text"><input type="number" name="vc" id="vc" min=0 value=0 style="width: 70px; text-align: center;"> Vector Calculus</p>
+                    </div>
+
+                    <!-- CATEGORY 2 -->
+                    <div style="background-color: orange; width: 33.3%; height: 100%;">
+                        <p class="mathematical-subject-text"><input type="number" name="f" id="f" min=0 value=0 style="width: 70px; text-align: center;"> Functions</p>
+                        <p class="mathematical-subject-text"><input type="number" name="ba" id="ba" min=0 value=0 style="width: 70px; text-align: center;"> Boolean Algebra</p>
+                        <p class="mathematical-subject-text"><input type="number" name="la" id="la" min=0 value=0 style="width: 70px; text-align: center;"> Linear Algebra</p>
+                    </div>
+
+                    <!-- CATEGORY 3 -->
+                    <div style="background-color: red; width: 33.3%; height: 100%;">
+                        <p class="mathematical-subject-text"><input type="number" name="a" id="a" min=0 value=0 style="width: 70px; text-align: center;"> Arithmetic</p>
+                        <p class="mathematical-subject-text"><input type="number" name="t" id="t" min=0 value=0 style="width: 70px; text-align: center;"> Trigonometry</p>
+                        <p class="mathematical-subject-text"><input type="number" name="s" id="s" min=0 value=0 style="width: 70px; text-align: center;"> Statistics</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- SETTINGS -->
+            <div style="width: 25%; height: 100%; background-color: purple;">
+                <p style="margin-top: 13%;"><input type="number" name="time" id="time" min=30 value=120 style="width: 70px; text-align: center;"> s</p>
+            </div>
+        </div>
+
+        
+    </div>
+</div>
+
+<!-- <div id="parent-container">
+    <div style="width: 100vw; height: 100vh;">
+        <div>
+            <div class="code-container" style="margin: 1vh 1vw 1vh 1vw; background-color: white;">
+                <p class="title">Join Code: {gameCode}</p>
+            </div>
+        </div>
+        
+
+
+
+
+
 
         <div class="player-list-menu" style="background-color: white;">
             <p class="title">Player:</p>
             <div class="player-grid">
                 {#each players as player (player.id ?? player.name)}
-                <p class="player-name">{player.name}</p>
+                <p style="margin: 0;">{player.name}</p>
                 {/each}
             </div>
         </div>
-        <div class="info-menu-container">
+
+
+
+
+
+
+        <div class="info-menu-container" style="background-color: white;">
             <div class="question-types">
                 <p class="title">Question Types:</p>
                 <div class="sections">
@@ -37,30 +127,37 @@
                     </div>
                 </div>
             </div>
+
+
+
+
+
             <div class="settings">
                 <p class="title">Settings:</p>
-                <p><input type="number" name="time" id="time" min=30 value=120>s</p>
+                <p style="margin-top: 13%;"><input type="number" name="time" id="time" min=30 value=120>s</p>
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 <style>
     * {
         font-size: 1.5rem;
     }
 
+    #parent-container {
+        background-color: black;
+        width: 100vw;
+        height: 100vh;
+    }
+
     .player-grid {
         display: grid;
         grid-template-columns: repeat(4, minmax(0, 1fr)); /* max 4 per row */
         gap: 12px; /* space between rows/cols */
+        
     }
 
-    .player-name {
-        margin: 0;
-    }
-
-    /* Optional: make it responsive */
     @media (max-width: 800px) {
         .player-grid { grid-template-columns: repeat(2, minmax(0, 1fr)); }
     }
@@ -68,80 +165,82 @@
         .player-grid { grid-template-columns: 1fr; }
     }
 
-    .code-container {
+    #code-container {
         display:flex;
         justify-content: center;
         align-items: center;
-        width: 100vw;
+        width: 98vw;
         height: 10vh;
+        background-color: white;
+        margin-left: 1vw;
+        transform: translateY(1vh);
     }
 
-    .player-list-menu{
-        width:100vw;
-        height:40vh;
+    .player-list-menu {
+        width: 98vw;
+        height: 40vh;
+        margin-left: 1vw;
+        background-color: white;
+        transform: translateY(2vh);
     }
 
-    .player-list-menu .sections{
+    .info-menu-container {
         display: flex;
-        justify-content: space-around;
         flex-direction: column;
-        gap:40px;
+        align-items: center;
+        width: 98vw;
+        height: 46vh;
+        margin-left: 1vw;
+        background-color: white;
+        transform: translateY(3vh);
     }
 
-    .player-list-menu .section{
+    #questions-menu {
+        width: 100%; 
+        height: 100%;
         display: flex;
         flex-direction: row;
-        justify-content: space-around;
-        width: 100vw;
+        align-items: center;
     }
 
-    .player-list-menu .section p{
-        display:flex;
-        width: 25vw;
+    .text-container {
+        background-color: red; 
+        width: 30%; 
+        height: 80%;
+        display: flex;
         justify-content: center;
+        align-items: center;
     }
 
-    .info-menu-container{
-        display:flex;
-        flex-direction: row;
-        width:100vw;
-        height:50vh;
+    .mathematical-subject-text {
+        margin: 15% 0 15% 0;
     }
 
-    .question-types{
-        width:75vw;
-        height:100%;
-    }
-
-    .title{
-        font-size: 2rem;
-    }
-
-    .info-menu-container .sections{
+    .info-menu-container .sections {
         margin-left: 10px;
-        display:flex;
+        display: flex;
         flex-direction: row;
     }
 
     .info-menu-container .section{
-        width:33%;
-        height:100%;
+        width: 33%;
+        height: 100%;
     }
 
-    .info-menu-container .section p:not(:last-child){
-        margin-bottom: 33%;
+    .info-menu-container .section p {
+        margin: 13% 0 25% 13%;
     }
 
-    input{
-        width:3vw;
+    input {
+        width: 3vw;
     }
 
-    .settings{
-        width:25vw;
-        height:100%;
+    .settings {
+        width: 25vw;
+        height: 100%;
     }
 
-    #time{
+    #time {
         width: 4vw;
     }
 </style>
